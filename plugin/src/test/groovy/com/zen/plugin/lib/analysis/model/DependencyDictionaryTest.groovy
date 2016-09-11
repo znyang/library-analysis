41e2cebc044145ae6d835aa99b74919be7f89b52
znyang <xggcl@sina.com>
2016-09-11 20:22:25 +0800

    关闭cache，恢复测试

--- plugin/src/test/groovy/com/zen/plugin/lib/analysis/model/DependencyDictionaryTest.groovy
+++ plugin/src/test/groovy/com/zen/plugin/lib/analysis/model/DependencyDictionaryTest.groovy
@@ -69,10 +69,9 @@ class DependencyDictionaryTest extends GMockTestCase {
             assert dictionary != null
             fileInfo.eachWithIndex { depend, index ->
                 def info = dictionary.findDependencyInfo(depend.id)
-//                assert info != null
-//                assert info.type.equals('aar')
-//                assert info.size == depend.size
-//                assert info.id.equals(depend.id)
+                assert info.type.equals('aar')
+                assert info.size == depend.size
+                assert info.id.equals(depend.id)
             }
         }
     }
