bac19bcbc538ffe3cf52ff9f1d487c619b20dcdd
znyang <xggcl@sina.com>
2016-09-21 23:32:40 +0800

    解决test错误

--- plugin/src/test/groovy/com/zen/plugin/lib/analysis/model/NodeTest.groovy
+++ plugin/src/test/groovy/com/zen/plugin/lib/analysis/model/NodeTest.groovy
@@ -4,8 +4,6 @@ import com.zen.plugin.lib.analysis.ext.LibraryAnalysisExtension
 import com.zen.plugin.lib.analysis.mock.DependencyGenerator
 import com.zen.plugin.lib.analysis.util.Logger
 import org.gmock.GMockTestCase
-import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.diagnostics.internal.graph.nodes.RenderableDependency
 import org.junit.Before
 import org.junit.Test
 
@@ -16,27 +14,32 @@ import org.junit.Test
 
 class NodeTest extends GMockTestCase {
 
-    RenderableDependency     data
-    DependencyDictionary     dictionary
-    DependencyGenerator      generator
-    LibraryAnalysisExtension extension
+    def root
+    def dictionary
+    def generator
+    def extension
+    def fileCollection
 
     @Override
     @Before
     void setUp() {
         super.setUp()
-        generator = new DependencyGenerator(gMockController)
-        data = generator.create()
-
-        def fileCollection = generator.getFileCollection()
-        dictionary = new DependencyDictionary(fileCollection)
         extension = new LibraryAnalysisExtension()
+        generator = new DependencyGenerator(gMockController)
+        root = generator.mockRootDependency()
+        fileCollection = generator.mockFileCollection()
+    }
+
+    private void prePlay() {
+        dictionary = new DependencyDictionary(fileCollection)
     }
 
     @Test
     void testData() {
         play {
-            Node node = Node.create(data)
+            prePlay()
+
+            Node node = Node.create(root)
             node.supplyInfo(extension, dictionary)
 
             node.children.each {
