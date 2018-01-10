269753537fdfef7255b190e079c00e1120231944
zenyang <xggcl@sina.com>
2018-01-10 18:05:58 +0800

    示例补充；文档更新

--- analysis-sample/src/main/java/com/zen/android/analysis/sample/BaseActivity.java
+++ analysis-sample/src/main/java/com/zen/android/analysis/sample/BaseActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Subscriber;
+import rx.functions.Func1;
 
 /**
  * @author zen
@@ -35,9 +38,12 @@ public abstract class BaseActivity extends Activity {
         if (ready == null) {
             ready = InitManager
                     .ready()
-                    .map(result -> {
-                        afterCreate(state);
-                        return result;
+                    .map(new Func1<Object, Object>() {
+                        @Override
+                        public Object call(Object result) {
+                            afterCreate(state);
+                            return result;
+                        }
                     });
         }
     }
@@ -68,10 +74,13 @@ public abstract class BaseActivity extends Activity {
     }
 
     protected Observable<?> genObsResume() {
-        return Observable.create(subscriber -> {
-            onObsResume();
-            subscriber.onNext(null);
-            subscriber.onCompleted();
+        return Observable.create(new OnSubscribe<Object>() {
+            @Override
+            public void call(Subscriber<? super Object> subscriber) {
+                BaseActivity.this.onObsResume();
+                subscriber.onNext(null);
+                subscriber.onCompleted();
+            }
         });
     }
 
